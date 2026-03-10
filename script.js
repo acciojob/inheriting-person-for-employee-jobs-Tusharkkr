eb7c281140081bc5b8f1e595aa7a9cbd83a97b88
Tushar kumar <narutoboi41@gmail.com>
2026-03-10 21:57:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 // complete this js code
-function Person(name, age) {
+class Person{
+    constructor(name, age){
         this.name = name
         this.age = age
-    
+    }
     greet(){
         console.log(`Hello, my name is ${this.name}, I am ${this.age} years old`)
     }
 }
 
-function Employee(name, age, jobTitle) {
-	// constructor(name, age, jobTitle){
-        super()
-        this.name = name
-        this.age = age
+class Employee extends Person{
+    constructor(name, age, jobTitle){
+        super(name,age)
+        // this.name = name
+        // this.age = age
         this.jobTitle = jobTitle
-    // }
+    }
     jobGreet(){
         console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
     }
