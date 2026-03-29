b37ff973c5015dd24df4378bda6f8945b3331400
Tushar kumar <narutoboi41@gmail.com>
2026-03-29 14:52:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,3 +19,6 @@ class Employee extends Person{
         console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
     }
 }
+// Do not change code below this line
+window.Person = Person;
+window.Employee = Employee;
